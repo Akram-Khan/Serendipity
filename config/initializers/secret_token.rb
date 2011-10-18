428b52b2cd526419bcc019a2cafd9c89a41bf1c2
Akram Khan <akramkhan83@gmail.com>
2011-10-18 18:20:32 +0530

    Initial Layout

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Serendipity::Application.config.secret_token = '2f0ae9beb7e8218af6a3cb312a2ac758c4cdcc1599d63538ad529c57b7c09c63931acae68e69a763336c9a77ffdba50e0015fdc0ee058466b30712d56431c3cf'
